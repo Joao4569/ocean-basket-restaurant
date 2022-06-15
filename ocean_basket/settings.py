30d88b08577d7ba8a0f9927024255ade0af8bca0
Joao4569 <johnnyrod4569@gmail.com>
2022-06-15 14:45:47 +0000

    feat: Create Heroku app, attach postgreSQL database and prepare environment and settings.py files

--- ocean_basket/settings.py
+++ ocean_basket/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+"""
+Import the os and dj_database_url libraries for referencing the env.py file
+and set a coditional statement to prevent errors due to how the Code Institute
+IDE template is designed.
+"""
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1ide@01j9hu3lzyt@1k_+xlt9qwnhw(t(bdzkcra=*2ga7ias='
+# Value for secret key variable set to get key as setup in env.py
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,11 +84,16 @@ WSGI_APPLICATION = 'ocean_basket.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
