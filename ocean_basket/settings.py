dfbdc8c7a4ba12787b8e3d2f0bec24d0ed566087
Joao4569 <johnnyrod4569@gmail.com>
2022-06-18 11:15:16 +0000

    chore: Add Heroku app as a permitted local host

--- ocean_basket/settings.py
+++ ocean_basket/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ocean-basket.herokuapp.com', 'localhost']
 
 
 # Application definition
