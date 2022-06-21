aeb3b051ceb7a98f8c29a8ccc2e4e9016cedb9f3
Joao4569 <johnnyrod4569@gmail.com>
2022-06-21 13:45:14 +0000

    feat: Create CustomerBookingDetails model and migrate

--- online_booking/models.py
+++ online_booking/models.py
@@ -1,3 +1,17 @@
+"""
+Import models folder in order to create tables for our database
+"""
 from django.db import models
 
-# Create your models here.
+
+class CustomerBookingDetails(models.Model):
+    """
+    Create a database model for online customer booking details
+    """
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=False, blank=False)
+    contact_number = models.IntegerField(null=False, blank=False)
+    email = models.CharField(max_length=50)
+    number_of_seats = models.SmallIntegerField(null=False, blank=False)
+    service = models.CharField(max_length=6, null=False, blank=False)
+    date = models.DateField(null=False, blank=False)
