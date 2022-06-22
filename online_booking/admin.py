4daf1905110f2b13f027298b4855c8bebe889808
Joao4569 <johnnyrod4569@gmail.com>
2022-06-22 08:28:39 +0000

    feat: Register CustomerBookingDetails model in admin.py file

--- online_booking/admin.py
+++ online_booking/admin.py
@@ -1,5 +1,5 @@
 """
-FIRST IMPORT HERE!!!!!!
+Standard Django built-in imports plus custom import as follows:
 Import CustomerBookingDetails model from models.py to register model
 """
 from django.contrib import admin
