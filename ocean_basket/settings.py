6e14fe9809f133f467ac6515449e12eea934ba66
Joao4569 <johnnyrod4569@gmail.com>
2022-06-22 13:01:39 +0000

    chore: Attend to problems highlighted by gitpod in settings.py file

--- ocean_basket/settings.py
+++ ocean_basket/settings.py
@@ -8,18 +8,18 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
-"""
 
-from pathlib import Path
-"""
+Modification:
 Import the os and dj_database_url libraries for referencing the env.py file
 and set a coditional statement to prevent errors due to how the Code Institute
 IDE template is designed.
 """
+from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
