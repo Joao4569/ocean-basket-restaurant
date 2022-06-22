36b53e4cc738720118aede1c28c1479e0f8ac4a1
Joao4569 <johnnyrod4569@gmail.com>
2022-06-22 13:31:47 +0000

    chore: Create url path and view function for home page, with minor changes to home_page.html

--- ocean_basket/urls.py
+++ ocean_basket/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-#from online_booking.views import
+from online_booking.views import get_home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_home_page, name='Home'),
 ]
--- online_booking/views.py
+++ online_booking/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def get_home_page(request):
+    """This function will display the home page"""
+    return render(request, 'online_booking/home_page.html')
