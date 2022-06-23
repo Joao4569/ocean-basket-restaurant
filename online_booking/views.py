476cc3e6ce9a75e05558b32602de7d6a7b2cdf30
Joao4569 <johnnyrod4569@gmail.com>
2022-06-23 14:13:42 +0000

    feat: Create base.html and link to home_page.html using templating

--- online_booking/views.py
+++ online_booking/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import CustomerBookingDetails
 
 # Create your views here.
 
 
 def get_home_page(request):
     """This function will display the home page"""
-    return render(request, 'online_booking/home_page.html')
+    bookings = CustomerBookingDetails.objects.all()
+    context = {
+        'bookings': bookings
+    }
+    return render(request, 'online_booking/home_page.html', context)
