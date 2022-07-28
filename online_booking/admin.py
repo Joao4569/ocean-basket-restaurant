31cfcc2094be56b159ede92683547ec3837b5982
Joao4569 <johnnyrod4569@gmail.com>
2022-07-28 11:02:35 +0000

    feat: Create 2 new models and delete old model

--- online_booking/admin.py
+++ online_booking/admin.py
@@ -3,7 +3,7 @@ Standard Django built-in imports plus custom import as follows:
 Import CustomerBookingDetails model from models.py to register model
 """
 from django.contrib import admin
-from .models import CustomerBookingDetails
+from .models import CustomerDetails
 
 
-admin.site.register(CustomerBookingDetails)
+admin.site.register(CustomerDetails)
--- online_booking/models.py
+++ online_booking/models.py
@@ -2,16 +2,37 @@
 Import models folder in order to create tables for our database
 """
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class CustomerBookingDetails(models.Model):
+class CustomerDetails(models.Model):
     """
-    Create a database model for online customer booking details
+    Create a database model for storing online customer details
     """
     first_name = models.CharField(max_length=30, null=False, blank=False)
     last_name = models.CharField(max_length=30, null=False, blank=False)
-    contact_number = models.IntegerField(null=False, blank=False)
+    contact_number = models.CharField(max_length=12, null=False, blank=False)
     email = models.CharField(max_length=50)
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_username")
+    password = models.CharField(max_length=50)
+
+    # def __str__(self):
+    # """This will modify the standard Django string method for
+    # personalisation"""
+    # return (f'{self.first_name} {self.last_name} table for '
+    # f'{self.number_of_seats} on {self.date}')
+
+
+class BookingInformation(models.Model):
+    """
+    Create a database model for storing online booking details
+    """
+    username = models.CharField(max_length=50)
+    booking_title = models.CharField(max_length=50)
+    number_of_seats = models.SmallIntegerField(null=False, blank=False)
+    date = models.DateField(null=False, blank=False)
+    contact_number = models.CharField(max_length=12, null=False, blank=False)
 
     # Choices field for selection of lunch or dinner service
     LUNCH = 'LH'
@@ -25,11 +46,3 @@ class CustomerBookingDetails(models.Model):
         max_length=2,
         choices=SERVICE_CHOICES,
     )
-    date = models.DateField(null=False, blank=False)
-    number_of_seats = models.SmallIntegerField(null=False, blank=False)
-
-    def __str__(self):
-        """This will modify the standard Django string method for
-        personalisation"""
-        return (f'{self.first_name} {self.last_name} table for '
-                f'{self.number_of_seats} on {self.date}')
