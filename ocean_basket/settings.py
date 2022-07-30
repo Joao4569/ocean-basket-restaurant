0fc9fe618305fbdd18aaf054e179b8359566df74
Joao4569 <johnnyrod4569@gmail.com>
2022-07-30 13:48:51 +0000

    feat: Add fields to django allauth signup form for customisation of storage in the user model of django

--- ocean_basket/settings.py
+++ ocean_basket/settings.py
@@ -161,3 +161,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_FORMS = {
+    'signup': 'online_booking.forms.CustomSignupForm',
+}
--- online_booking/forms.py
+++ online_booking/forms.py
@@ -1,5 +1,23 @@
 from django import forms
+from allauth.account.forms import SignupForm
 from .models import CustomerDetails
+
+
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(
+        max_length=25, label='First Name')
+    last_name = forms.CharField(
+        max_length=25, label='Last Name')
+    contact_number = forms.CharField(
+        max_length=12, label='Contact Number')
+
+    def save(self, request):
+        user = super(CustomSignupForm, self).save(request)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.contact_number = self.cleaned_data['contact_number']
+        user.save()
+        return user
 
 
 class RegistrationForm(forms.ModelForm):
