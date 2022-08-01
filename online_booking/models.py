78d5b14ba6353e546a0ad8e11d2a2a76ced67411
Joao4569 <johnnyrod4569@gmail.com>
2022-08-01 14:18:21 +0000

    style: Edit default django string method for models for better UX on admin site

--- online_booking/models.py
+++ online_booking/models.py
@@ -3,11 +3,6 @@ Import models folder in order to create tables for our database
 """
 from django.db import models
 from django.contrib.auth.models import User
-# def __str__(self):
-# """This will modify the standard Django string method for
-# personalisation"""
-# return (f'{self.first_name} {self.last_name} table for '
-# f'{self.number_of_seats} on {self.date}')
 
 
 class BookingInformation(models.Model):
@@ -31,3 +26,9 @@ class BookingInformation(models.Model):
     number_of_seats = models.SmallIntegerField(null=False, blank=False)
     date = models.DateField(null=False, blank=False)
     contact_number = models.CharField(max_length=12, null=False, blank=False)
+
+    def __str__(self):
+        """This will modify the standard Django string method for
+        personalisation"""
+        return (f'{self.booking_title} {self.contact_number} table for '
+                f'{self.number_of_seats} on {self.date}')
