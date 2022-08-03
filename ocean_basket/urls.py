ae86a1082942e1759cbe0edc8c21e17009028483
Joao4569 <johnnyrod4569@gmail.com>
2022-08-03 14:18:55 +0000

    feat: Create edit_booking.html with view, url, form and redirect

--- ocean_basket/urls.py
+++ ocean_basket/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('create_booking/', views.CreateBooking.as_view(), name='Create Booking'),
     path('accounts/', include('allauth.urls')),
     path('view_booking/', views.ViewBooking.as_view(), name='View Booking'),
+    path('edit_booking/<booking_id>', views.EditBooking.as_view(), name='Edit Booking'),
 ]
--- online_booking/views.py
+++ online_booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .forms import BookingForm
 from .models import BookingInformation
@@ -34,9 +34,13 @@ class CreateBooking(View):
         else:
             booking_form = BookingForm()
 
-        return render(
-            request,
-            "online_booking/view_booking.html",
+        bookings = BookingInformation.objects.all()
+        context = {
+            'bookings': bookings
+        }
+
+        return render(request,
+            "online_booking/view_booking.html", context
         )
 
 
@@ -50,3 +54,34 @@ class ViewBooking(View):
             'bookings': bookings
         }
         return render(request, "online_booking/view_booking.html", context)
+
+
+class EditBooking(View):
+    """This function will display the edit booking page"""
+    def get(self, request, booking_id):
+        """This function will display the home page"""
+        booking_id = get_object_or_404(BookingInformation, id=booking_id)
+        edit_form = {
+                "edit_form": BookingForm(instance=booking_id)
+            }
+        return render(request, "online_booking/edit_booking.html", edit_form)
+
+    def post(self, request, booking_id):
+        edit_form = BookingForm(data=request.POST)
+
+        if edit_form.is_valid():
+            edit_form.instance.username = request.user.username
+            edit_form.instance.pk = booking_id
+            edit_form.save()
+        else:
+            edit_form = BookingForm()
+
+        bookings = BookingInformation.objects.all()
+        context = {
+            'bookings': bookings
+        }
+
+        return render(
+            request,
+            "online_booking/view_booking.html", context
+        )
