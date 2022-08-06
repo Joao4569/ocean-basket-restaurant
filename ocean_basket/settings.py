3bfbe6c63b5eb70960fe03a349bf8e8443fc5ec9
Joao4569 <johnnyrod4569@gmail.com>
2022-08-06 08:17:17 +0000

    fix: Fix stylesheet link error for deployment

--- ocean_basket/settings.py
+++ ocean_basket/settings.py
@@ -36,6 +36,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['ocean-basket.herokuapp.com', 'localhost']
 
 
