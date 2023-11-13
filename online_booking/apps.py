0cb3c30c4ac6f8105a9ed59c8dcb9503e490f464
João Rodrigues <johnnyrod4569@gmail.com>
2023-11-13 11:31:37 +0100

    chore: Update gitignore file

--- online_booking/apps.py
+++
@@ -1,8 +0,0 @@
-"""Import AppConfig from django apps"""
-from django.apps import AppConfig
-
-
-class OnlineBookingConfig(AppConfig):
-    """Allocate auto field for django model and name app"""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'online_booking'
